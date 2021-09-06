9c3f2d541ba71a330fdbb4953f0e1c24ed25eb67
= <=>
2021-09-06 21:44:42 +0700

    Config Spring MVC to expose User photos directory

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MVCConfig.java
@@ -0,0 +1,22 @@
+package com.shopme.admin;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String dirName = "user-photos";
+        //Lấy ra ảnh của user
+        Path userPhotosDir = Paths.get(dirName);
+        //lấy ra đường dẫn của ảnh
+        String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
+        registry.addResourceHandler("/" + dirName + "/**")
+                .addResourceLocations("file:/" + userPhotosPath + "/");
+
+    }
+}
